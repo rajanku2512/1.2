4fba9325fe840d0ff104d19d2a680f7a2d0c3c5c
rajanku2512 <rajanku2512@gmail.com>
2016-12-23 02:26:41 +0530

    Add files via upload

---
+++ Assignment12.java
@@ -0,0 +1,21 @@
+
+public class Assignment12 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int a=20;
+int b=10;
+
+int c=a--;
+int d=a>>2;
+int e= a&b;
+ b=a-- - --a;
+System.out.println(a);
+System.out.println(b);
+System.out.println(c);
+System.out.println(d);
+System.out.println(e);
+
+	}
+
+}
